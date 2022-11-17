3fb2c732b5a89d4cd3209b90b5c30ab6830b3c47
jrymer1 <jrymer1@gmail.com>
2022-11-17 10:38:54 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import cytoscape from "cytoscape";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  React.useEffect(() => {
+    const cy = cytoscape({
+      container: document.getElementById("cy"),
+      elements: [
+        // list of graph elements to start with
+        {
+          // node a
+          data: { id: "a" },
+        },
+        {
+          // node b
+          data: { id: "b" },
+        },
+        {
+          // edge ab
+          data: { id: "ab", source: "a", target: "b", lineStyle: "dashed" },
+        },
+      ],
+      style: [
+        // the stylesheet for the graph
+        {
+          selector: "node",
+          style: {
+            "background-color": "#666",
+            label: "data(id)",
+          },
+        },
+
+        {
+          selector: "edge",
+          style: {
+            width: 3,
+            "line-color": "#ccc",
+            "target-arrow-color": "#ccc",
+            "target-arrow-shape": "triangle",
+            "curve-style": "bezier",
+            "line-style": "data(lineStyle)" as cytoscape.Css.LineStyle, // If we don't type cast here, we get: Type '"data(lineStyle)"' is not assignable to type 'PropertyValueEdge<LineStyle> | undefined'
+          },
+        },
+      ],
+
+      layout: {
+        name: "grid",
+        rows: 1,
+      },
+    });
+
+    /** 
+     * I am trying to achieve function syntax in the stylesheet to hopefully skirt around the above type casting I am forced to do.
+     * Using the example code found here: https://js.cytoscape.org/#style the following should be possible (and would hopefully be fully typed)
+        style: cytoscape.stylesheet()
+          .selector('node')
+            .style({
+              'background-color': function( ele ){ return ele.data('bg') }
+          })
+     * cy.style().selector and cytoscape().style().selector returns a string
+     * import {stylesheet} from 'cytoscape' yields '"cytoscape"' has no exported member named 'stylesheet'. Did you mean 'Stylesheet'? Which is the interface
+     * cy.stylesheet() does not exist
+     * 
+     * https://js.cytoscape.org/#cy.style suggests cy.style().selector() should be a function that accepts a string
+     * cytoscape/index.d.ts CoreStyle says selector is a string
+    */
+  }, []);
+
+  return <div id="cy" style={{ height: "100vh", width: "100%" }} />;
 }
 
 export default App;
